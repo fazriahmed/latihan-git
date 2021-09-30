8ec494f9ea6efd3013e7877091ecff6673a4e786
WP2_Fazri ahmed <91084539+fazriahmed@users.noreply.github.com>
2021-09-30 16:11:26 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+  {
+        echo "<h1>Perkenalkan</>";
+        echo"nama saya fazri ahmed
+            saya tinggal di daerah kabes
+            olahraga yang saya sukai adalah
+            futsal";
+  }
+}  
