010f3dfe1682a110828da3b2142658a7904db1f7
WP2_Fazri ahmed <91084539+fazriahmed@users.noreply.github.com>
2021-10-18 13:44:21 +0700

    Add files via upload

--- Matakuliah.php
+++ Matakuliah.php
@@ -4,18 +4,45 @@ class Matakuliah extends CI_Controller
 
     public function index()
     {
-
+        $this->load->view('tamplate/header');
+        $this->load->view('tamplate/footer');
         $this->load->view('view-form-matakuliah');
     }
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
+        $this->load->view('tamplate/header');
+        $this->load->view('tamplate/footer');
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Kode Matakuliah Harus diisi',
+                'min_length' => 'Kode terlalu pendek'
+            ]
+        );
 
-        $this->load->view('matkul/view-data-matakuliah', $data);
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Nama Matakuliah Harus diisi',
+                'min_length' => 'Nama terlalu pendek'
+            ]
+        );
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
